566a5c7752a58d5441305b347306781a8c7d5bf9
Rohit Joseph <rohitjosephrhea285@gmail.com>
2023-12-15 14:16:29 +0530

    module file modified

--- modules/addition.py
+++
@@ -1,4 +0,0 @@
-def add1(num1,num2):
-    return(num1+num2)
-    
-    
--- modules/function.py
+++
@@ -1,8 +0,0 @@
-from modules.addition import add1
-    
-num1 = int(input("Enter num1: "))
-
-num2 = int(input("Enter num2: "))
-
-result=add1(num1,num2) 
-print(result)
