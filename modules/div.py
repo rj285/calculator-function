4c64460f942dc2feb2e8cca761e6eedbe49deefe
Rohit Joseph <rohitjosephrhea285@gmail.com>
2023-12-15 20:24:45 +0530

    bug fixed

--- modules/div.py
+++ modules/div.py
@@ -1,16 +1,15 @@
-# #rohit-joseph 
+#rohit-joseph 
 
-# def div1(value_1,value_2):
-#     if value_2==0:
-#         print("Error!!! cant devide with Zero")
-#     elif value_1==0:
-#         print("Error!!! cant devide with Zero")
-#     else:
-#         result = value_1 / value_2
-#         print(f"{value_1} / {value_2} is: {result}")
-#     return(value_1 / value_2)
+def div1(value_1,value_2):
+    if value_2==0:
+        print("Error!!! cant devide with Zero")
+    elif value_1==0:
+        print("Error!!! cant devide with Zero")
+    else:
+        result = value_1 / value_2
+        print(f"{value_1} / {value_2} is: {result}")
+    return(value_1 / value_2)
 
     
-num=183527
+
     
-print(len(num))
